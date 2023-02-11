e57b120c411615be78ea9e95b03fa5067a434664
sbroo <sbrookes916@gmail.com>
2023-02-11 11:21:17 +0000

    added main and qr screen

---
+++ flutter files/lib/qrScreen.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class qrScreen extends StatefulWidget {
+
+
+  qrScreen(
+      {Key? key})
+      : super(key: key);
+
+  @override
+  State<qrScreen> createState() => qrScreenState();
+}
+
+class qrScreenState extends State<qrScreen> {
+  late TextEditingController _controller;
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
