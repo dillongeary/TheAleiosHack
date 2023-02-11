fe96610969e02ed8debb93cea7a14ab6538b7007
sbroo <sbrookes916@gmail.com>
2023-02-11 15:43:12 +0000

    added map functionality that doesnt work

--- flutter files/lib/mapScreen.dart
+++ flutter files/lib/mapScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter_osm_plugin/flutter_osm_plugin.dart'
+import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map/plugin_api.dart';
+import 'package:latlong2/latlong.dart';
 class mapScreen extends StatefulWidget {
 
 
@@ -16,9 +18,34 @@ class mapScreenState extends State<mapScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-
-    );
+    return Scaffold();
   }
+    /*
+    return FlutterMap(
+      options: MapOptions(
+        center: LatLng(51.509364, -0.128928),
+        zoom: 9.2,
+      ),
+      nonRotatedChildren: [
+        AttributionWidget.defaultWidget(
+          source: 'OpenStreetMap contributors',
+          onSourceTapped: null,
+        ),
+      ],
+
+      children: [
+        TileLayer(
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+          userAgentPackageName: 'com.example.app',
+        ),
+      ],
+    );
+
+
+  }
+
+     */
+
 }
+
 
