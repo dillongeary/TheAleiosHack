ec6fbfbaadbee9a17a6a5a675f524d2027ac330e
sbroo <sbrookes916@gmail.com>
2023-02-11 17:46:12 +0000

    added learderboard code

--- flutter files/lib/leaderboardScreen.dart
+++ flutter files/lib/leaderboardScreen.dart
@@ -1,41 +1,47 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class leaderboardScreen extends StatefulWidget {
-
-
-  leaderboardScreen(
-      {Key? key})
-      : super(key: key);
+  leaderboardScreen({Key? key}) : super(key: key);
 
   @override
   State<leaderboardScreen> createState() => leaderboardScreenState();
 }
 
 class leaderboardScreenState extends State<leaderboardScreen> {
+  Map<String, int> lb = new Map();
+  bool _loading = true;
+
+  @override
+  void initState() {
+    getLeaderboard();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        body: _loading
+            ? Center(
+                child: CircularProgressIndicator(
+                valueColor: new AlwaysStoppedAnimation<Color>(
+                    Colors.blue), //choose your own color
+              ))
+            : Container());
+  }
 
-    );
+  void getLeaderboard() async {
+    FirebaseFirestore db = FirebaseFirestore.instance;
+
+    await db.collection("Leaderboard").get().then((var querySnapshot) {
+      querySnapshot.docs.forEach((var doc) {
+        final data = doc.data() as Map<String, dynamic>;
+        String name = data["name"];
+        int points = data["points"];
+        lb[name] = points;
+      });
+    }, onError: (e) => print("Error getting document: $e"));
+    print(lb);
+    _loading = false;
   }
 }
-
-
-  
-  Map<String, int> getLeaderboard() async {
-    FirebaseFirestore db = FirebaseFirestore.instance;
-    Map<String, int> lb = new Map();
-    final docRef = db.collection("Leaderboard").docs();
-    await docRef.get().then((var doc) {
-      for (var i; doc) {
-        final data = i.data() as Map<String, dynamic>;
-        name = data["name"];
-        points = data["points"];
-        lb[name] = points;
-      } 
-    },onError: (e) => print("Error getting document: $e")
-    );
-    return lb
-  }
